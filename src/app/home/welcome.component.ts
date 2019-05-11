d30f21c30ce2b5dc06d17dab2698b7cf63e73cbd
Alex <alexandre.barbez@gmail.com>
2019-05-11 10:27:16 +0200

    Ajoute des cases a cocher sur la page welcome pour aider lea dans son projet.

--- src/app/home/welcome.component.ts
+++ src/app/home/welcome.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import {FormControl} from "@angular/forms";
 
 @Component({
   templateUrl: './welcome.component.html'
 })
 export class WelcomeComponent {
   public pageTitle = 'Welcome';
+
+  options = [
+    {name:'OptionA', value:'1', checked:true},
+    {name:'OptionB', value:'2', checked:false},
+    {name:'OptionC', value:'3', checked:true}
+  ]
+
+  get selectedOptions() { // right now: ['1','3']
+    return this.options
+        .filter(opt => opt.checked)
+        .map(opt => opt.value)
+  }
+
+  printChecked() {
+    console.log(this.selectedOptions);
+  }
 }
